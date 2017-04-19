3ac42ecd1b6a4d7a356bbd1c2ca541bf246a35f0
Admin <Admin@DESKTOP-1O7PIA8>
2017-04-19 13:24:47 +0200

    Wersja 2.7

--- src/apk/Window/MainWindow.java
+++ src/apk/Window/MainWindow.java
@@ -708,6 +708,7 @@ public class MainWindow extends javax.swing.JFrame  {
             }
         }
         repaint();
+        
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
